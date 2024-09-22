a877c0f1365a5cc190a3d1e52c87babc9fc0f9dc
Mangkidals <mrasyalawi@gmail.com>
2024-09-23 03:19:13 +0700

    Fifth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_tester/bottomnavigation_bar.dart';
 import 'package:flutter_tester/bottomsheet_widget.dart';
 import 'package:flutter_tester/container_page.dart';
 import 'package:flutter_tester/center.dart';
@@ -9,6 +10,7 @@ import 'package:flutter_tester/datepicker_widget.dart';
 import 'package:flutter_tester/dialog_widget.dart';
 import 'package:flutter_tester/listview_builder.dart';
 import 'package:flutter_tester/navigationpush_widget.dart';
+import 'package:flutter_tester/navigationpop_widget.dart';
 import 'package:flutter_tester/padding.dart';
 import 'package:flutter_tester/radiobutton.dart';
 import 'package:flutter_tester/row.dart';
@@ -16,6 +18,7 @@ import 'package:flutter_tester/stack.dart';
 import 'package:flutter_tester/sized_box.dart';
 import 'package:flutter_tester/switch.dart';
 import 'package:flutter_tester/snackbar_widget.dart';
+import 'package:flutter_tester/tabbar_widget.dart';
 import 'package:flutter_tester/textview.dart';
 import 'package:flutter_tester/wrapsigma.dart';
 
@@ -35,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: RadiobuttonMyWidget(),
+      home: NavigationpushMyWidget(),
     );
   }
 }
